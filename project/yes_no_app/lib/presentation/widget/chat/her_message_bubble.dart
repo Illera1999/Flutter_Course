de3d37472a2dc0d522424025ea7780800f57b47e
Illera1999 <illerarodriguez1999@gmail.com>
2025-03-09 22:14:41 +0000

    Fin de la aplicación de chat, primeros pasos con Flutter

--- project/yes_no_app/lib/presentation/widget/chat/her_message_bubble.dart
+++ project/yes_no_app/lib/presentation/widget/chat/her_message_bubble.dart
@@ -19,13 +19,13 @@ class HerMessageBubble extends StatelessWidget {
             color: colors.secondary,
             borderRadius: BorderRadius.circular(20),
           ),
-          child: const Padding(
+          child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Text('Como estas?', style: TextStyle(color: Colors.white)),
+            child: Text(message.text, style: TextStyle(color: Colors.white)),
           ),
         ),
         const SizedBox(height: 5),
-        _ImageBubble(),
+        if (message.imageUrl != null) _ImageBubble(message.imageUrl!),
         const SizedBox(height: 10),
       ],
     );
@@ -33,13 +33,18 @@ class HerMessageBubble extends StatelessWidget {
 }
 
 class _ImageBubble extends StatelessWidget {
+
+  const _ImageBubble(this.image);
+
+  final String image;
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return ClipRRect(
       borderRadius: BorderRadius.circular(20),
       child: Image.network(
-        'https://yesno.wtf/assets/no/3-80a6f5b5d6684674bcfeda34accca4e1.gif',
+        image,
         width: size.width * 0.7,
         height: 150,
         fit: BoxFit.cover,
