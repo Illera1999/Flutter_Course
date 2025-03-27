0b7cfea98f74893b5afb3d3f5b73418ffba20366
Illera1999 <illerarodriguez1999@gmail.com>
2025-03-27 22:33:23 +0000

    Mapper

---
+++ project/toktik/lib/infrastructure/models/local_video_model.dart
@@ -0,0 +1,30 @@
+import 'package:toktik/domain/entities/video_post.dart';
+
+class LocalVideoModel {
+  final String name;
+  final String videoUrl;
+  final int likes;
+  final int video;
+
+  const LocalVideoModel({
+    required this.name,
+    required this.videoUrl,
+    this.likes = 0,
+    this.video = 0,
+  });
+
+  factory LocalVideoModel.fromJson(Map<String, dynamic> json) =>
+      LocalVideoModel(
+        name: json['name'] ?? 'No name',
+        videoUrl: json['videoUrl'],
+        likes: json['likes'] ?? 0,
+        video: json['video'] ?? 0,
+      );
+
+  Map<String, dynamic> toJson() {
+    return {'name': name, 'videoUrl': videoUrl, 'likes': likes, 'video': video};
+  }
+
+  VideoPost toVideoPostEntity() =>
+      VideoPost(caption: name, videoUrl: videoUrl, likes: likes, views: video);
+}
