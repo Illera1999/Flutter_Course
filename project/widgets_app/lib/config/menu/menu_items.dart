e49506ce5a4fbcdab2e2e41f642f0d0c734e76d7
Illera1999 <illerarodriguez1999@gmail.com>
2025-04-19 20:57:15 +0100

    Animated Container

--- project/widgets_app/lib/config/menu/menu_items.dart
+++ project/widgets_app/lib/config/menu/menu_items.dart
@@ -42,4 +42,10 @@ class MenuItems {
       link: '/snackbars',
       icon: Icons.info_outline,
     ),
+    MenuItems(
+      title: 'Animated container',
+      subTitle: 'Stateful widget animado',
+      link: '/animated',
+      icon: Icons.check_box_outline_blank_rounded,
+    ),
   ];
