369938b0cefbbe3d8081939bd1b5aeff2f13c264
Illera1999 <illerarodriguez1999@gmail.com>
2025-05-28 23:32:29 +0100

    GoRouter oficial implenetado

--- project/cinemapedia/lib/config/router/app_router.dart
+++ project/cinemapedia/lib/config/router/app_router.dart
@@ -1,23 +1,55 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (cootext, state) => const HomeScreen(),
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
       routes: [
         GoRoute(
-          path: 'movie/:id',
-          name: MovieScreen.name,
+          path: '/',
           builder: (context, state) {
-            final movieID = state.pathParameters['id'];
-            return MovieScreen(moviesId: movieID ?? 'no id');
+            return const HomeView();
+          },
+          routes: [
+            GoRoute(
+              path: 'movie/:id',
+              name: MovieScreen.name,
+              builder: (context, state) {
+                final movieID = state.pathParameters['id'];
+                return MovieScreen(moviesId: movieID ?? 'no id');
+              },
+            ),
+          ],
+        ),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoriteView();
           },
         ),
       ],
     ),
   ],
 );
+
+    // Rutas padre/hijo
+    // GoRoute(
+    //   path: '/',
+    //   name: HomeScreen.name,
+    //   builder: (cootext, state) => const HomeScreen(childView: FavoriteView(),),
+    //   routes: [
+    //     GoRoute(
+    //       path: 'movie/:id',
+    //       name: MovieScreen.name,
+    //       builder: (context, state) {
+    //         final movieID = state.pathParameters['id'];
+    //         return MovieScreen(moviesId: movieID ?? 'no id');
+    //       },
+    //     ),
+    //   ],
+    // ),
--- project/cinemapedia/lib/presentation/widgets/shared/custom_botton_navigation.dart
+++ project/cinemapedia/lib/presentation/widgets/shared/custom_botton_navigation.dart
@@ -1,11 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottonNavigation extends StatelessWidget {
+
+  int getCurrentIndex(BuildContext context) {
+    // Forma de resaltar donde estamos en el appbar
+    final currentUri = GoRouter.of(context).routeInformationProvider.value.uri;
+    switch (currentUri.path) {
+      case '/':
+        return 0; 
+      case '/categories':
+        return 1; 
+      case '/favorites':
+        return 2; 
+      default:
+        return 0; 
+    }
+  }
+
+  void _onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        // Navigate to Home
+        context.go('/');
+        break;
+      case 1:
+        // Navigate to Categories
+        context.go('/categories');
+        break;
+      case 2:
+        // Navigate to Favorites
+        context.go('/favorites');
+        break;
+      default:
+        break;
+    }
+  }
   const CustomBottonNavigation({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      elevation: 0,
+      currentIndex: getCurrentIndex(context),
+      onTap: (value) => _onItemTapped(context, value),
       items: [
         BottomNavigationBarItem(
           icon: Icon(Icons.home),
