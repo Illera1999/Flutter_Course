d3ee665adc4434581b7fba44d5fae2364400bebd
Illera1999 <illerarodriguez1999@gmail.com>
2025-06-10 19:16:14 +0100

    SomeFix

--- project/push_app/lib/config/router/app_router.dart
+++ project/push_app/lib/config/router/app_router.dart
@@ -1,17 +1,22 @@
-
-
-
 import 'package:go_router/go_router.dart';
-import 'package:push_app/presentation/screen/details_screen.dart';
 import 'package:push_app/presentation/screen/screen.dart';
+
+
 
 final appRouter = GoRouter(
   routes: [
-    GoRoute(path: '/',
+
+
+    GoRoute(
+      path: '/',
       builder: (context, state) => const HomeScreen(),
     ),
-    GoRoute(path: '/push-details/:messageId',
-      builder: (context, state) => DetailsScreen(pushMessageId: state.pathParameters['messageId'] ?? '',),
+
+    GoRoute(
+      path: '/push-details/:messageId',
+      builder: (context, state) => DetailsScreen( pushMessageId: state.pathParameters['messageId'] ?? '', ),
     ),
-  ],
+
+  ]
+  
 );
--- project/push_app/lib/domain/entities/push_message.dart
+++ project/push_app/lib/domain/entities/push_message.dart
@@ -1,29 +1,32 @@
 class PushMessage {
+
   final String messageId;
   final String title;
   final String body;
   final DateTime sentDate;
-  final Map<String, dynamic>? data;
+  final Map<String,dynamic>? data;
   final String? imageUrl;
 
   PushMessage({
-    required this.messageId,
-    required this.title,
-    required this.body,
-    required this.sentDate,
-    this.data,
-    this.imageUrl,
+    required this.messageId, 
+    required this.title, 
+    required this.body, 
+    required this.sentDate, 
+    this.data, 
+    this.imageUrl
   });
 
   @override
   String toString() {
-    return ''' 
-    messageId:  $messageId;
-    title:  $title;
-    body:  $body;
-    sentDate:  $sentDate;
-    data:  $data;
-    imageUrl:  $imageUrl;
-    ''';
+    return '''
+PushMessage - 
+  id:    $messageId
+  title: $title
+  body:  $body
+  data:  $data
+  imageUrl: $imageUrl
+  sentDate: $sentDate
+''';
   }
+
 }
