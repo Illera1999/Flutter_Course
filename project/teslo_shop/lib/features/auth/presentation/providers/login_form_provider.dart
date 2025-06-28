d5cf915f141895e2ee36390f03889af96a91de96
Illera1999 <illerarodriguez1999@gmail.com>
2025-06-28 20:12:32 +0100

    Login with riverpod

---
+++ project/teslo_shop/lib/features/auth/presentation/providers/login_form_provider.dart
@@ -0,0 +1,106 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:formz/formz.dart';
+import 'package:teslo_shop/features/shared/shared.dart';
+
+
+
+//! 3- StateotifierProvider - consume afuera
+final loginFormProvider = StateNotifierProvider.autoDispose<LoginFormNotifier, LoginFormState>((ref) {
+  return LoginFormNotifier();
+});
+
+//! 2- Como implementamos un notifier
+class LoginFormNotifier extends StateNotifier<LoginFormState> {
+  LoginFormNotifier(): super(LoginFormState());
+
+
+  void onEmailChanged(String value) {
+    final email = Email.dirty(value);
+    state = state.copyWith(
+      email: email,
+      isValid: Formz.validate([
+        email,
+        state.password,
+      ]),
+    );
+  }
+  void onPasswordChanged(String value) {
+    final password = Password.dirty(value);
+    state = state.copyWith(
+      password: password,
+      isValid: Formz.validate([
+        state.email,
+        password,
+      ]),
+    );
+  }
+
+  void onFormSubmit() {
+    _touchEveryField();
+    if ( !state.isValid ) return;
+    print(state);
+  }
+  void _touchEveryField() {
+    final email = Email.dirty(state.email.value);
+    final password = Password.dirty(state.password.value);
+    
+    state = state.copyWith(
+      isFormposted: true,
+      email: email,
+      password: password,
+      isValid: Formz.validate([
+        email,
+        password,
+      ]),
+    );
+  }
+  
+}
+
+//! 1- State del procider
+class LoginFormState {
+  final bool isPosting;
+  final bool isFormposted;
+  final bool isValid;
+  final Email email;
+  final Password password;
+
+  LoginFormState({
+    this.isPosting = false,
+    this.isFormposted = false,
+    this.isValid = false,
+    this.email = const Email.pure(),
+    this.password = const Password.pure(),
+  });
+
+  LoginFormState copyWith({
+    bool? isPosting,
+    bool? isFormposted,
+    bool? isValid,
+    Email? email,
+    Password? password,
+  }) {
+    return LoginFormState(
+      isPosting: isPosting ?? this.isPosting,
+      isFormposted: isFormposted ?? this.isFormposted,
+      isValid: isValid ?? this.isValid,
+      email: email ?? this.email,
+      password: password ?? this.password,
+    );
+  }
+
+  @override
+  String toString() {
+    return '''
+      LoginFormState(
+        isPosting: $isPosting,
+        isFormposted: $isFormposted,
+        isValid: $isValid,
+        email: $email,
+        password: $password
+      )''';
+  }
+}
+
+
+
